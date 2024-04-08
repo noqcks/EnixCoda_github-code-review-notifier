13a5c9ba82283f9c2e40268fba726d032a5e7535
noqcks <benji@093b.org>
2024-04-08 02:44:01 +0000

    Upgrade dependencies

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,5 @@
-import firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/database'
 import { firebaseConfig } from './config'
 import { dePromiseLike } from './utils'
 
